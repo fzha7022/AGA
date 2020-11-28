26c9fa0aa1021dd6a15b5a4799daf1fd2c5f1435
fengying zhang <garyzzzzzzzzz@gmail.com>
2020-11-28 12:31:11 +0000

    ,

--- script.js
+++
@@ -1,7 +0,0 @@
-function classToggle() {
-    this.classList.toggle('class1');
-    this.classList.toggle('class2');
-
-}
-
-document.getElementById('container').addEventListener('click', classToggle);
